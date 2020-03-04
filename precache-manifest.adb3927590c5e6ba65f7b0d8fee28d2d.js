8bf2c9d68a5f3203f47575acaeea62de348e25af
lbeatu <ozhan.yldz@gmail.com>
2020-03-04 18:49:02 +0300

    Updates

---
+++ precache-manifest.adb3927590c5e6ba65f7b0d8fee28d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f05730410cdbb470d2ce8c5fefba24",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "2c6ae5c201eb9d247102",
+    "url": "/me/static/css/main.e27ebdcb.chunk.css"
+  },
+  {
+    "revision": "e0f6a15edd6a8dc8ddc9",
+    "url": "/me/static/js/2.e82702b9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/me/static/js/2.e82702b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6ae5c201eb9d247102",
+    "url": "/me/static/js/main.fb0f7e69.chunk.js"
+  },
+  {
+    "revision": "f4b4d1cd804f7f104cf1",
+    "url": "/me/static/js/runtime-main.809d3c69.js"
+  }
+]);
